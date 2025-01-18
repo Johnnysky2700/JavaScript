bf485ec39fdd26ab09acedac38f2de042b771da9
iamjohnnysky <vanpasie20@gmail.com>
2025-01-18 13:47:13 -0800

    IF statement

--- text.js
+++ text.js
@@ -3,3 +3,15 @@ alert("Hello, world!");
 
 // the second alert
 alert("Hello, world!");
+
+
+
+var youLikeMeat = false;
+var myNum = 8;
+
+if (myNum == 10){
+     document.write("myNum is equal to 10");
+
+} else{
+    
+}
