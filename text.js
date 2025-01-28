67cf111c4fcc78dd5bcc2dfcdfd21ea072634909
iamjohnnysky <vanpasie20@gmail.com>
2025-01-28 12:15:53 -0800

    variable scope

--- text.js
+++ text.js
@@ -57,3 +57,23 @@ function myFunction() {
     lastName = "Doe";
     lastname = "Peterson";
     document.getElementById("demo6").innerHTML = lastName;
+
+    var average = 0;
+
+    function getAverage (a,b){
+       
+         average = (a + b) / 2; //local variable
+         console.log(average);
+         return average;
+
+    }
+
+    var myResult = getAverage(7,11); //gobal variable
+
+    function logResult(){
+
+           console.log("the average is " + myResult + "inside the function");
+
+    }
+
+    logResult();
