d5fbf41efdad5c7cd4f1b59f0c1fcd4333a12229
iamjohnnysky <vanpasie20@gmail.com>
2025-01-30 15:27:32 -0800

    using 'THIS' keyword

--- text.js
+++ text.js
@@ -8,24 +8,30 @@
    
     } 
 
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello";
-
-var myCar = new Object();
-myCar.maxspeed = 50;
-myCar.driver = "johnnysky";
-myCar.drive = function(){ console.log("now driving");};
-
-myCar.drive();
 
 var myCar2 = {
       maxSpeed: 70, 
       driver: "net ninja",
       drive: function(speed, time){
             console.log(speed * time);
-      } 
+      }, 
+      test: function(){
+            console.log(this);
+      }
 };
 
+var myCar3 = {
+      maxSpeed: 70, 
+      driver: "johnnysky",
+      drive: function(speed, time){
+            console.log(speed * time);
+      }, 
+      logDriver: function(){
+            console.log("driver name is " +  this.driver)
+      }
+};
+
+myCar2.test();
+myCar3.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
