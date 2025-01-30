0509d52941f02c60f21036cbc183117da4d847ce
iamjohnnysky <vanpasie20@gmail.com>
2025-01-30 14:57:07 -0800

    creating a new object and in it short form

--- text.js
+++ text.js
@@ -8,5 +8,23 @@
    
     } 
 
-    var myString = 'i\'m a "fun" string';
-    console.log(myString.indexOf("string"));
+var myArray = new Array();
+myArray[0] = 8;
+myArray[1] = "hello";
+
+var myCar = new Object();
+myCar.maxspeed = 50;
+myCar.driver = "johnnysky";
+myCar.drive = function(){ console.log("now driving");};
+
+myCar.drive();
+
+var myCar2 = {
+      maxSpeed: 70, 
+      driver: "net ninja",
+      drive: function(){
+            console.log("now driving again");} 
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive();
