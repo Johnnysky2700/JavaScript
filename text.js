d8fc4da9eb7fe125026e6e63e941dcf6fadd6812
iamjohnnysky <vanpasie20@gmail.com>
2025-02-02 11:11:39 -0800

    Completed onclick event example

--- text.js
+++ text.js
@@ -1,20 +1,14 @@
-    var a = "hello";
-    var b = 5;
-
-    //doudle negative
-    if (! isNaN(a)){
-
-          console.log("meaning of life is " + (a * b));
-   
-    } 
-
 var content = document.getElementById("content")
-var button = document.getElementById("show more")
+var button = document.getElementById("show-more")
 
 button.onclick = function(){
             if(content.className == "open"){
                 //shrink the box
+                content.className = "";
+                button.innerHTML = "Show More";
          } else{
               //expand the box
+              content.className = "open";
+              button.innerHTML = "Show Less";
       }
 };
